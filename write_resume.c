16a4a66050bcc46ccadaf4cfaa2523208248834d
Sam Zofkie <samzofkie@gmail.com>
2023-06-04 16:06:01 -0500

    Began refactoring out most structs

--- write_resume.c
+++ write_resume.c
@@ -9,7 +9,7 @@
 // * section / bullet func
 // * beautify all (const members of struct)
 
-
+/*
 enum Alignment {Left, Right, Center};
 
 typedef struct {
@@ -47,7 +47,7 @@ static Document doc = {
     NULL,   // surface
     NULL,   // cr
     0,      // cursor
-    NULL,
+    NULL,   // head
     {
       {"Cantarell Bold", 26, Center, false, 15, true}, // Title
       {"Cantarell", 12, Center, false, 15, true},      // Subtitle
@@ -59,7 +59,6 @@ static Document doc = {
       {"Cantarell", 10, Left, false, 1, true}          // Bullet
     }
 };
-
 
 void init_doc(void)
 {
@@ -210,12 +209,76 @@ void cleanup_doc(void)
   cairo_destroy(doc.cr);
   cairo_surface_destroy(doc.surface); 
 }
+*/
 
 
-int main (int argc, char **argv)
-{
-  init_doc();
+static const double doc_width = 8.5*72,
+                    doc_height = 11*72,
+                    margin = 25,
+                    cursor = margin;
+static cairo_surface_t *surface;
+static cairo_t *cr;
 
+
+void Title(const char *str) {
+  PangoLayout *layout = pango_cairo_create_layout(cr);
+  PangoFontDescription *font_desc = pango_font_description_from_string("Cantarell Bold");
+  
+  pango_layout_set_text(layout, str, -1); 
+  pango_font_description_set_size(font_desc, 26 * PANGO_SCALE);
+  pango_layout_set_font_description(layout, font_desc);
+  pango_font_description_free(font_desc);
+
+  int _w, _h;
+  pango_layout_get_size(layout, &_w, &_h);
+  double w = (double)_w / PANGO_SCALE;
+  double h = (double)_h / PANGO_SCALE;
+
+  double x = margin, 
+         y = cursor;
+
+  /*if (layout_width > doc_width - margin * 2)
+    return write_long_line(t);
+  
+  if (style.alignment == Center)
+    x = (doc.width - layout_width) / 2;
+  
+  else if (style.alignment == Right)
+    x = doc.width - doc.margin - layout_width;
+   
+  if (style.section_line) {
+    double line_y = y + layout_height/2 + 2;
+    cairo_move_to(doc.cr, x + layout_width, line_y);
+    cairo_line_to(doc.cr, doc.width - doc.margin, line_y);
+    cairo_stroke(doc.cr); 
+  }*/
+  
+  cairo_move_to(cr, x, y);
+  pango_cairo_show_layout(cr, layout);
+  g_object_unref(layout);
+
+  /*if (style.newline)
+    doc.cursor += layout_height + style.cursor_increment;*/
+
+};
+
+void Subtitle(const char *str);
+void SectionTitle(const char *str);
+void SplitLine(const char *left, const char *right);
+void SubSectionTitle(const char *str);
+void Bullet(const char *str);
+
+
+int main (int argc, char **argv) {
+  surface = cairo_pdf_surface_create("resume.pdf", doc_width, doc_height);
+  cr = cairo_create(surface);
+  cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+  cairo_paint(cr);
+  cairo_set_source_rgb(cr, 0, 0, 0);
+  cairo_set_line_width(cr, 0.5);
+
+  Title("Sam Zofkie");
+  /*
   add("Sam Zofkie", Title);
   add("samzofkie@gmail.com  •  github.com/samzofkie  •  samzofkie.com", Subtitle);
   add("Education", SectionTitle);
@@ -231,11 +294,10 @@ int main (int argc, char **argv)
   add("Skills", SectionTitle);
   add("Proficient: C, C++, JavaScript, React, Git, Unix", Normal12);
   add("Familiar: Python, Bash, Docker, SQL, AWS, HTML, CSS", Normal12);
+  */
 
-  for (Text *p=doc.head; p; p=p->next)
-    write(p);
-    
-  cleanup_doc();
+  cairo_destroy(cr);
+  cairo_surface_destroy(surface); 
 
   return 0;
 }
