6a9ecb76018c7799030a6cee4b8d4ed5948e98f6
Sam Zofkie <samzofkie@gmail.com>
2023-06-05 09:06:20 -0500

    SectionTitle function

--- write_resume.c
+++ write_resume.c
@@ -3,14 +3,7 @@
 #include <cairo-pdf.h>
 #include <pango/pangocairo.h>
 
-// TODO
-// * refactor line wrap
-// * section / bullet func
-// * beautify all (const members of struct)
-
 /*
-enum Alignment {Left, Right, Center};
-
 typedef struct {
   const char *font_string;
   int font_size;
@@ -20,33 +13,6 @@ typedef struct {
   bool newline;
 } TextStyle;
 
-enum TextStyleType {Title, Subtitle, SectionTitle,
-                    ProjectTitle, Normal12, NoNewline12,
-                    Right12, Bullet};
-
-typedef struct _Text {
-  char *text; 
-  enum TextStyleType style_type;
-  struct _Text *next;
-} Text;
-
-typedef struct {
-  double width, height, margin; 
-  cairo_surface_t *surface;
-  cairo_t *cr;
-  double cursor;
-  Text *head;
-  TextStyle styles[];
-} Document;
-
-static Document doc = { 
-    8.5*72, // width 
-    11*72,  // height
-    25,     // margin
-    NULL,   // surface
-    NULL,   // cr
-    0,      // cursor
-    NULL,   // head
     {
       {"Cantarell Bold", 26, Center, false, 15, true}, // Title
       {"Cantarell", 12, Center, false, 15, true},      // Subtitle
@@ -58,62 +24,6 @@ static Document doc = {
       {"Cantarell", 10, Left, false, 1, true}          // Bullet
     }
 };
-
-void init_doc(void)
-{
-  doc.cursor = doc.margin;
-
-  doc.surface = cairo_pdf_surface_create("resume.pdf", doc.width, doc.height);
-  doc.cr = cairo_create(doc.surface);
-
-  cairo_set_source_rgb(doc.cr, 1.0, 1.0, 1.0);
-  cairo_paint(doc.cr);
-  cairo_set_source_rgb(doc.cr, 0, 0, 0);
-  cairo_set_line_width(doc.cr, 0.5);
-}
-
-
-TextStyle *get_style(Text *t)
-{
-  return &doc.styles[t->style_type];
-}
-
-
-PangoLayout *create_layout_get_size(Text *t, double *w, double *h)
-{
-  PangoLayout *layout = pango_cairo_create_layout(doc.cr);
-  PangoFontDescription *font_desc = pango_font_description_from_string(get_style(t)->font_string);
-  
-  pango_layout_set_text(layout, t->text, -1); 
-  pango_font_description_set_size(font_desc, get_style(t)->font_size * PANGO_SCALE);
-  pango_layout_set_font_description(layout, font_desc);
-  pango_font_description_free(font_desc);
-
-  int _w, _h;
-  pango_layout_get_size(layout, &_w, &_h);
-  *w = (double)_w / PANGO_SCALE;
-  *h = (double)_h / PANGO_SCALE;
-
-  return layout;
-}
-
-
-Text *alloc_text(const char *str, enum TextStyleType type)
-{
-  Text *new = (Text*)malloc(sizeof(Text));
-  new->text = (char*)malloc(strlen(str)+1);
-  strcpy(new->text, str);
-  new->style_type = type;
-  new->next = NULL;
-  return new;
-}
-
-
-void *dealloc_text(Text *t)
-{
-  free(t->text);
-  free(t);
-}
 
 
 void write_long_line(Text *t)
@@ -148,68 +58,10 @@ void write_long_line(Text *t)
 }
 
 
-void write(Text *t) {
-  PangoLayout *layout;
-  double layout_width, layout_height;
-  layout = create_layout_get_size(t, &layout_width, &layout_height);
-  double x = doc.margin, 
-         y = doc.cursor;
-  TextStyle style = *get_style(t);
 
-  if (layout_width > doc.width - doc.margin * 2)
-    return write_long_line(t);
-  
-  if (style.alignment == Center)
-    x = (doc.width - layout_width) / 2;
-  
   else if (style.alignment == Right)
     x = doc.width - doc.margin - layout_width;
-   
-  if (style.section_line) {
-    double line_y = y + layout_height/2 + 2;
-    cairo_move_to(doc.cr, x + layout_width, line_y);
-    cairo_line_to(doc.cr, doc.width - doc.margin, line_y);
-    cairo_stroke(doc.cr); 
-  }
-  
-  cairo_move_to(doc.cr, x, y);
-  pango_cairo_show_layout(doc.cr, layout);
-  g_object_unref(layout);
-
-  if (style.newline)
-    doc.cursor += layout_height + style.cursor_increment;
-}
-
-
-void add(const char *str, enum TextStyleType type)
-{
-  Text *new = alloc_text(str, type);
-
-  if (!doc.head) {
-    doc.head = new;
-    return;
-  }
-
-  Text *last_node = doc.head;
-  while (last_node->next)
-    last_node = last_node->next;
-  last_node->next = new;
-};
-
-
-void cleanup_doc(void)
-{
-  Text *curr = doc.head, *prev = doc.head;
-  while (curr) {
-    prev = curr;
-    curr = curr->next;
-    dealloc_text(prev);
-  }
-  cairo_destroy(doc.cr);
-  cairo_surface_destroy(doc.surface); 
-}
 */
-
 
 static const double doc_width = 8.5*72,
                     doc_height = 11*72,
@@ -223,7 +75,6 @@ typedef struct {
   PangoLayout *layout;
   double width, height;
 } TextLayout;
-
 
 TextLayout new_layout(const char *str, const char *font_str, int font_size) {
   PangoLayout *layout = pango_cairo_create_layout(cr);
@@ -240,7 +91,6 @@ TextLayout new_layout(const char *str, const char *font_str, int font_size) {
   return (TextLayout){layout, (double)w / PANGO_SCALE, (double)h / PANGO_SCALE};
 }
 
-
 void draw_and_free_layout(double x, double y, PangoLayout *l) {
   cairo_move_to(cr, x, y);
   pango_cairo_show_layout(cr, l);
@@ -250,6 +100,13 @@ void draw_and_free_layout(double x, double y, PangoLayout *l) {
 
 void Center(double *x, TextLayout l) {
   *x = (doc_width - l.width) / 2;
+}
+
+void SectionLine(double x, double y, TextLayout l) {
+  double line_y = y + l.height/2 + 2;
+  cairo_move_to(cr, x + l.width, line_y);
+  cairo_line_to(cr, doc_width - margin, line_y);
+  cairo_stroke(cr);
 }
 
 
@@ -264,7 +121,6 @@ void Title(const char *str) {
   cursor += text_layout.height + 15;
 };
 
-
 void Subtitle(const char *str) {
   double x = margin, y = cursor;
   TextLayout text_layout = new_layout(str, "Cantarell", 12);
@@ -276,8 +132,17 @@ void Subtitle(const char *str) {
   cursor += text_layout.height + 15;
 }
 
+void SectionTitle(const char *str) {
+  double x = margin, y = cursor;
+  TextLayout text_layout = new_layout(str, "Cantarell Bold", 18);
 
-void SectionTitle(const char *str);
+  SectionLine(x, y, text_layout);
+
+  draw_and_free_layout(x, y, text_layout.layout);
+
+  cursor += text_layout.height + 2;
+}
+
 void SplitLine(const char *left, const char *right);
 void SubSectionTitle(const char *str);
 void Bullet(const char *str);
@@ -293,10 +158,8 @@ int main (int argc, char **argv) {
 
   Title("Sam Zofkie");
   Subtitle("samzofkie@gmail.com  •  github.com/samzofkie  •  samzofkie.com");
+  SectionTitle("Education");
   /*
-  add("Sam Zofkie", Title);
-  add("samzofkie@gmail.com  •  github.com/samzofkie  •  samzofkie.com", Subtitle);
-  add("Education", SectionTitle);
   add("Reed College, B.A. Computer Science", NoNewline12);
   add("2016-2020", Right12);
   add("Projects", SectionTitle);
